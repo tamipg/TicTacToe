85348fdfa2041b575fcbd02f7cb544eadde48a16
Tamara <tami.pg94@gmail.com>
2022-10-25 21:52:16 +0200

    estructura

--- TicTacToe/Form1.Designer.vb
+++ TicTacToe/Form1.Designer.vb
@@ -22,10 +22,98 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Tablero = New System.Windows.Forms.TableLayoutPanel()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.Juego = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.AyudaButton = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Tablero
+        '
+        Me.Tablero.ColumnCount = 3
+        Me.Tablero.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.Tablero.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.Tablero.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.Tablero.Location = New System.Drawing.Point(12, 28)
+        Me.Tablero.Name = "Tablero"
+        Me.Tablero.RowCount = 3
+        Me.Tablero.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.Tablero.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.Tablero.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.Tablero.Size = New System.Drawing.Size(372, 313)
+        Me.Tablero.TabIndex = 0
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Juego, Me.ToolStripSeparator1, Me.AyudaButton})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(396, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'Juego
+        '
+        Me.Juego.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.Juego.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.Juego.Image = CType(resources.GetObject("Juego.Image"), System.Drawing.Image)
+        Me.Juego.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.Juego.Name = "Juego"
+        Me.Juego.Size = New System.Drawing.Size(51, 22)
+        Me.Juego.Text = "Juego"
+        '
+        'NuevoToolStripMenuItem
+        '
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'AyudaButton
+        '
+        Me.AyudaButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.AyudaButton.Image = CType(resources.GetObject("AyudaButton.Image"), System.Drawing.Image)
+        Me.AyudaButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.AyudaButton.Name = "AyudaButton"
+        Me.AyudaButton.Size = New System.Drawing.Size(45, 22)
+        Me.AyudaButton.Text = "Ayuda"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(396, 448)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.Tablero)
+        Me.Name = "Form1"
+        Me.Text = "Tic Tac Toe"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Tablero As TableLayoutPanel
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents Juego As ToolStripDropDownButton
+    Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents AyudaButton As ToolStripButton
 End Class
--- TicTacToe/Form1.vb
+++ TicTacToe/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿' Implementar el conocido juego del 3 en raya.
+' En esta primera versión deberemos implementar:
+' - Las nueve casilla creadas en tiempo de ejecución y formando un 3x3
+' - Menu que permita salir del juego, iniciar uno nuevo y tener una ayuda.
+' - Jugaremos 2 humanos, asi que cada clic pondrá una imagen distinta en una casilla vacía.
+' - El sistema elegirá de forma aleatoria que jugador (1 ó 2) es el primero en jugar e indicará quien ha ganado
+'   o si ha terminado en empate, dando la posibilidad de volver a jugar o salir.
 
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
