a9c83e4e8d51acb48702a45fee06f2192bc0761b
Tamara <tami.pg94@gmail.com>
2022-11-05 02:20:25 +0100

    tic tac toe 1

--- TicTacToe/Form1.Designer.vb
+++ TicTacToe/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Tablero = New System.Windows.Forms.TableLayoutPanel()
@@ -30,6 +30,10 @@ Partial Class Form1
         Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.AyudaButton = New System.Windows.Forms.ToolStripButton()
+        Me.jugador1Label = New System.Windows.Forms.Label()
+        Me.Jugador2Label = New System.Windows.Forms.Label()
+        Me.puntuacion1 = New System.Windows.Forms.Label()
+        Me.Puntuacion2 = New System.Windows.Forms.Label()
         Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -39,13 +43,13 @@ Partial Class Form1
         Me.Tablero.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
         Me.Tablero.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
         Me.Tablero.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
-        Me.Tablero.Location = New System.Drawing.Point(12, 28)
+        Me.Tablero.Location = New System.Drawing.Point(16, 106)
         Me.Tablero.Name = "Tablero"
         Me.Tablero.RowCount = 3
         Me.Tablero.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
         Me.Tablero.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
         Me.Tablero.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
-        Me.Tablero.Size = New System.Drawing.Size(372, 313)
+        Me.Tablero.Size = New System.Drawing.Size(330, 330)
         Me.Tablero.TabIndex = 0
         '
         'ToolStrip1
@@ -53,7 +57,7 @@ Partial Class Form1
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Juego, Me.ToolStripSeparator1, Me.AyudaButton})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(396, 25)
+        Me.ToolStrip1.Size = New System.Drawing.Size(363, 25)
         Me.ToolStrip1.TabIndex = 1
         Me.ToolStrip1.Text = "ToolStrip1"
         '
@@ -70,13 +74,13 @@ Partial Class Form1
         'NuevoToolStripMenuItem
         '
         Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
-        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
         Me.NuevoToolStripMenuItem.Text = "Nuevo"
         '
         'SalirToolStripMenuItem
         '
         Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
-        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
         Me.SalirToolStripMenuItem.Text = "Salir"
         '
         'ToolStripSeparator1
@@ -93,13 +97,59 @@ Partial Class Form1
         Me.AyudaButton.Size = New System.Drawing.Size(45, 22)
         Me.AyudaButton.Text = "Ayuda"
         '
+        'jugador1Label
+        '
+        Me.jugador1Label.AutoSize = True
+        Me.jugador1Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.jugador1Label.Location = New System.Drawing.Point(12, 40)
+        Me.jugador1Label.Name = "jugador1Label"
+        Me.jugador1Label.Size = New System.Drawing.Size(107, 24)
+        Me.jugador1Label.TabIndex = 0
+        Me.jugador1Label.Text = "JugadorX:"
+        '
+        'Jugador2Label
+        '
+        Me.Jugador2Label.AutoSize = True
+        Me.Jugador2Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jugador2Label.Location = New System.Drawing.Point(12, 73)
+        Me.Jugador2Label.Name = "Jugador2Label"
+        Me.Jugador2Label.Size = New System.Drawing.Size(108, 24)
+        Me.Jugador2Label.TabIndex = 2
+        Me.Jugador2Label.Text = "JugadorO:"
+        '
+        'puntuacion1
+        '
+        Me.puntuacion1.AutoSize = True
+        Me.puntuacion1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.puntuacion1.Location = New System.Drawing.Point(115, 40)
+        Me.puntuacion1.Name = "puntuacion1"
+        Me.puntuacion1.Size = New System.Drawing.Size(21, 24)
+        Me.puntuacion1.TabIndex = 3
+        Me.puntuacion1.Text = "0"
+        '
+        'Puntuacion2
+        '
+        Me.Puntuacion2.AutoSize = True
+        Me.Puntuacion2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Puntuacion2.Location = New System.Drawing.Point(115, 73)
+        Me.Puntuacion2.Name = "Puntuacion2"
+        Me.Puntuacion2.Size = New System.Drawing.Size(21, 24)
+        Me.Puntuacion2.TabIndex = 4
+        Me.Puntuacion2.Text = "0"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(396, 448)
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(363, 448)
+        Me.Controls.Add(Me.Puntuacion2)
+        Me.Controls.Add(Me.puntuacion1)
+        Me.Controls.Add(Me.Jugador2Label)
+        Me.Controls.Add(Me.jugador1Label)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.Tablero)
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "Tic Tac Toe"
         Me.ToolStrip1.ResumeLayout(False)
@@ -116,4 +166,8 @@ Partial Class Form1
     Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
     Friend WithEvents AyudaButton As ToolStripButton
+    Friend WithEvents jugador1Label As Label
+    Friend WithEvents Jugador2Label As Label
+    Friend WithEvents puntuacion1 As Label
+    Friend WithEvents Puntuacion2 As Label
 End Class
--- TicTacToe/Form1.vb
+++ TicTacToe/Form1.vb
@@ -7,7 +7,262 @@
 '   o si ha terminado en empate, dando la posibilidad de volver a jugar o salir.
 
 Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Dim checker As Boolean
+    Dim btnTic1 As String
+    Dim btnTic2 As String
+    Dim btnTic3 As String
+    Dim btnTic4 As String
+    Dim btnTic5 As String
+    Dim btnTic6 As String
+    Dim btnTic7 As String
+    Dim btnTic8 As String
+    Dim btnTic9 As String
+    Dim plusone As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CargarTablero()
+    End Sub
+
+
+    Private Sub CargarTablero()
+
+        Dim j As Integer
+        For j = 1 To 9
+            Dim casilla As New System.Windows.Forms.Button
+            With casilla
+                .Name = "btnTic" & j
+                .Left = .Width * (j - 1)
+                .Image = Image.FromFile(IO.Path.Combine(IO.Path.GetDirectoryName(IO.Path.GetDirectoryName(Application.StartupPath)), "imagenes\casilla.png"))
+                .Dock = DockStyle.Fill
+                .Parent = Me.Tablero
+                Controls.Add(casilla) 'Esto añade la LABEL al formulario
+                Tablero.Controls.Add(casilla)
+                AddHandler casilla.Click, AddressOf clickGame
+            End With
+        Next
+        checker = False
+    End Sub
+
+
+
+
+    Private Sub clickGame(sender As Button, e As MouseEventArgs) Handles Tablero.Click
+        Dim casilla As Button = sender
+
+        If (checker = False) Then
+            casilla.Image = Image.FromFile(IO.Path.Combine(IO.Path.GetDirectoryName(IO.Path.GetDirectoryName(Application.StartupPath)), "imagenes\o.png"))
+            checker = True
+        Else
+            casilla.Image = Image.FromFile(IO.Path.Combine(IO.Path.GetDirectoryName(IO.Path.GetDirectoryName(Application.StartupPath)), "imagenes\x.png"))
+            checker = False
+        End If
+        casilla.Enabled = False
+
+        Select Case casilla.Name
+            Case "btnTic1"
+                If checker = True Then
+                    btnTic1 = "O"
+                Else
+                    btnTic1 = "X"
+                End If
+
+            Case "btnTic2"
+                If checker = True Then
+                    btnTic2 = "O"
+                Else
+                    btnTic2 = "X"
+                End If
+
+            Case "btnTic3"
+                If checker = True Then
+                    btnTic3 = "O"
+                Else
+                    btnTic3 = "X"
+                End If
+            Case "btnTic4"
+                If checker = True Then
+                    btnTic4 = "O"
+                Else
+                    btnTic4 = "X"
+                End If
+            Case "btnTic5"
+                If checker = True Then
+                    btnTic5 = "O"
+                Else
+                    btnTic5 = "X"
+                End If
+            Case "btnTic6"
+                If checker = True Then
+                    btnTic6 = "O"
+                Else
+                    btnTic6 = "X"
+                End If
+            Case "btnTic7"
+                If checker = True Then
+                    btnTic7 = "O"
+                Else
+                    btnTic7 = "X"
+                End If
+            Case "btnTic8"
+                If checker = True Then
+                    btnTic8 = "O"
+                Else
+                    btnTic8 = "X"
+                End If
+            Case "btnTic9"
+                If checker = True Then
+                    btnTic9 = "O"
+                Else
+                    btnTic9 = "X"
+                End If
+
+        End Select
+
+        score(casilla)
+    End Sub
+
+    Private Sub BorrarTablero()
+        Tablero.Controls.Clear()
+        btnTic1 = ""
+        btnTic2 = ""
+        btnTic3 = ""
+        btnTic4 = ""
+        btnTic5 = ""
+        btnTic6 = ""
+        btnTic7 = ""
+        btnTic8 = ""
+        btnTic9 = ""
+    End Sub
+
+    Private Sub score(sender As Button)
+        Dim casilla As Button = sender
+
+        If (btnTic1 = "X" And btnTic2 = "X" And btnTic3 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic4 = "X" And btnTic5 = "X" And btnTic6 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic7 = "X" And btnTic8 = "X" And btnTic9 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic1 = "X" And btnTic4 = "X" And btnTic7 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic2 = "X" And btnTic5 = "X" And btnTic8 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic3 = "X" And btnTic6 = "X" And btnTic9 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic1 = "X" And btnTic5 = "X" And btnTic9 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic3 = "X" And btnTic5 = "X" And btnTic7 = "X") Then
+            MessageBox.Show("El ganador es el jugador X", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(puntuacion1.Text)
+            puntuacion1.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic1 = "O" And btnTic2 = "O" And btnTic3 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic4 = "O" And btnTic5 = "O" And btnTic6 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic7 = "O" And btnTic8 = "O" And btnTic9 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic1 = "O" And btnTic4 = "O" And btnTic7 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic2 = "O" And btnTic5 = "O" And btnTic8 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic3 = "O" And btnTic6 = "O" And btnTic9 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic1 = "O" And btnTic5 = "O" And btnTic9 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
+
+        If (btnTic3 = "O" And btnTic5 = "O" And btnTic7 = "O") Then
+            MessageBox.Show("El ganador es el jugador O", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            plusone = Convert.ToInt64(Puntuacion2.Text)
+            Puntuacion2.Text = Convert.ToString(plusone + 1)
+            BorrarTablero()
+            CargarTablero()
+        End If
     End Sub
 End Class
